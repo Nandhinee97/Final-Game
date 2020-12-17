37e5d05723494e9ed70f70cc2620d9cd2949d0eb
Nandhinee97 <knandhinee1@gmail.com>
2020-12-16 17:14:01 -0800

    level2(new)

---
+++ Assets/myscripts_level2/GMaster.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMaster : MonoBehaviour
+{
+
+
+
+    public Transform energyObj;
+    public Transform hurdle2;
+    public Transform Fox1;
+    public Transform greenbase;
+    public int randNumber;
+     public float zScenePos = 18;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+     Instantiate(greenbase,new Vector3(0,0,23),greenbase.rotation);  
+     Instantiate(greenbase,new Vector3(0,0,36),greenbase.rotation);  
+      Instantiate(greenbase,new Vector3(0,0,59),greenbase.rotation);  
+      Instantiate(greenbase,new Vector3(0,0,82),greenbase.rotation); 
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (zScenePos < 150 && zScenePos > 15)
+        {
+            randNumber = Random.Range(0, 10);
+            if (randNumber < 3)
+            {
+                if (randNumber == 0)
+                {
+                    Instantiate(energyObj, new Vector3(-2.5f, .5f, zScenePos), energyObj.rotation);
+                }
+                else if (randNumber == 1)
+                {
+                    Instantiate(energyObj, new Vector3(0, .5f, zScenePos), energyObj.rotation);
+                }
+                else if (randNumber == 2)
+                {
+                    Instantiate(energyObj, new Vector3(2.5f, .5f, zScenePos), energyObj.rotation);
+                }
+            }
+            if(randNumber == 3)
+            {
+                Instantiate(Fox1, new Vector3(-1.5f, 0, zScenePos), Fox1.rotation);
+            }
+            if (randNumber == 4)
+            {
+                Instantiate(hurdle2, new Vector3(0.0f, 0, zScenePos), hurdle2.rotation);
+            }
+            if (randNumber == 5)
+            {
+                Instantiate(Fox1, new Vector3(1.5f, 0, zScenePos), Fox1.rotation);
+            }
+            if (randNumber == 6)
+            {
+                Instantiate(hurdle2, new Vector3(-2.7f, 0, zScenePos), hurdle2.rotation);
+            }
+	    if (randNumber == 7)
+            {
+                Instantiate(Fox1, new Vector3(1.5f, 0, zScenePos), Fox1.rotation);
+            }	
+            if (randNumber == 8)
+            {
+                Instantiate(hurdle2, new Vector3(2.7f, 0, zScenePos), hurdle2.rotation);
+            }
+
+            
+            Instantiate(greenbase, new Vector3(0, 0, zScenePos), greenbase.rotation);
+            zScenePos += 6;
+        }
+        
+    }
+}
---
+++ Assets/myscripts_level2/movepen.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class movepen : MonoBehaviour
+{
+
+public KeyCode moveL;
+public KeyCode moveR;
+public KeyCode jump;
+
+public float horizVel = 0;
+public int laneNum = 4;
+public string controlLocked = "n";
+//public static int energyTotal = 0;
+
+public float jumpheight=7f;
+private Rigidbody rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		rb=  GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3 (horizVel,0,4);
+
+		if ((Input.GetKeyDown(moveL))&& (controlLocked == "n")&& (laneNum > 1))
+		{
+			horizVel = -2.5f;
+			StartCoroutine(stopSlide());
+			laneNum -= 1;
+			controlLocked = "y";
+		}
+		
+		if ((Input.GetKeyDown(moveR))&& (controlLocked == "n")&& (laneNum < 6))
+		{
+			horizVel = 2.5f;
+			StartCoroutine(stopSlide());
+			laneNum += 1;
+			controlLocked = "y";
+		}
+
+
+	if (Input.GetKeyDown(jump))
+{
+rb.AddForce(Vector3.up*jumpheight);
+}
+	
+	//if (Input.GetKey("space"))
+	  //    {
+       //GetComponent<Rigidbody>().velocity = new Vector3(0,6,5);
+        //StartCoroutine(stopJump());
+        //}
+}
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "lethal")                                             // ends sequence
+        {   
+	    //SoundManagerScript.PlaySound("collide");	
+	    
+            Destroy(gameObject);
+                                                                 // move to fail sequence
+        }
+	 if (other.gameObject.name == "energy(Clone)")                                             // ends sequence
+        {   
+	    //SoundManagerScript.PlaySound("collide");	
+	    
+            Destroy(other.gameObject);
+                                                                 // move to fail sequence
+        }
+	
+	if (other.gameObject.name == "Foxprefab(Clone)")                                             // ends sequence
+        {   
+	    //SoundManagerScript.PlaySound("collide");	
+	    
+            Destroy(gameObject);
+                                                                 // move to fail sequence
+        }
+    }
+
+
+IEnumerator stopSlide()
+{
+yield return new WaitForSeconds(.5f);
+horizVel = 0;
+controlLocked = "n";
+}
+
+//IEnumerator stopJump()
+  //  {
+    //    yield return new WaitForSeconds(0.5f);
+	//	GetComponent<Rigidbody>().velocity = new Vector3(0,-6,5);
+        //yield return new WaitForSeconds(0.5f);
+	//	GetComponent<Rigidbody>().velocity = new Vector3(0,0,5);
+    //}
+
+}
+
